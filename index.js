28a85246eef8135c733e9a27223dffd2686a082d
Rahmatillayeva <shaxrizodamrahmatillayeva@gmail.com>
2024-02-18 23:18:24 +0500

    yangi misol

---
+++ index.js
@@ -0,0 +1,52 @@
+// 1 - misol
+
+// let a = prompt("a = ");
+// console.log(`a = ${a}`);
+//  if (a%2 == 0 && a > 0) {
+//   console.log(`${a} musbat juft son.`);
+//  }
+//  if (a%2 == 0 && a < 0) {
+//   console.log(`${a} manfiy juft son.`);
+//  }
+//  if (a%2 != 0 && a > 0) {
+//   console.log(`${a} musbat toq son.`);
+//  }
+//  if (a%2 != 0 && a < 0) {
+//   console.log(`${a} manfiy toq son.`);
+//  }
+//  if (a == 0) {
+//   console.log(`kiritilgan son 0 ga teng.`);
+//  }
+
+// 2 - misol
+
+
+// let a = prompt("a = ");
+// console.log(`a = ${a}`);
+// if (a%2 == 0 && a/10 > 0 && a/10 < 10) {
+// console.log(`${a} ikki xonali juft son.`);
+// }
+// if (a%2 != 0 && a/10 > 0 && a/10 < 10) {
+// console.log(`${a} ikki xonali toq son.`);
+// }
+// if (a%2 == 0 && a/100 > 0 && a/100 < 10) {
+// console.log(`${a} uch xonali juft son.`);
+// }
+// if (a%2 != 0 && a/100 > 0 && a/100 < 10) {
+// console.log(`${a} uch xonali toq son.`);
+// }
+
+
+// 3 - misol
+// Avazbek universitetga o'qishga kirmoqchi. O'qishga kirish uchun IELTS - 5.5, math - 40 yoki "tankasi” bo'lishi kerak). Agar shartlar bajarilgan bo'lsa o'qishga kirdingiz, bo'lmasa kirolmadingiz deb consoleda ko'rsatsin.
+
+
+// let IELTS = prompt("IELTS darajangizni kiriting!"),
+// math = prompt("Matematikadan to'g'ri javoblar sonini kiriting!")
+// tanka = true;
+
+// if (IELTS >= 5.5 && (math > 40 || tanka)) {
+// console.log("Tabriklaymiz! Siz o'qishga kirdingiz.");
+// } else {
+//   console.log("Siz o'qishga kira olmadingiz.");
+// }
